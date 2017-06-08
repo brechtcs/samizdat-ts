192fd2a65bed36d687d1ef3d256c74aad2615260
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-06-09 00:50:52 +0200

    Split tests into separate files

--- test/serializable.js
+++ test/serializable.js
@@ -1,7 +1,7 @@
 var assert = require('assert')
 var levelup = require('levelup')
 var memdown = require('memdown')
-var samizdat = require('./')
+var samizdat = require('../')
 
 var level = levelup({db: memdown})
 var test = samizdat(level)
@@ -61,40 +61,6 @@ test.add(slug, v1, function (err, data) {
       test.rm(data.key, function (err) {
         assert.deepEqual(err, {entryUpdated: true}, 'Removing updated entry does not return correct error')
         console.info('Success: older version of updated entry cannot be removed')
-      })
-    })
-  })
-})
-
-/**
- * Add, open & remove string entry
- */
-test.add('more', 'stuff', function (err, data) {
-  if (err) {
-    console.error(err)
-  }
-  assert.ok(!err, 'Failed to create string entry')
-  console.info('Success: created string entry')
-
-  test.open(data.key, function (err, entry) {
-    if (err) {
-      console.error(err)
-    }
-    assert.ok(!err, 'Failed to open entry')
-    assert.equal(entry, 'stuff', 'Opened string entry value wrong')
-    console.info('Success: opened string entry')
-
-    test.rm(data.key, function (err, entry) {
-      if (err) {
-        console.error(err)
-      }
-      assert.ok(!err, 'Failed to remove entry')
-      assert.equal(entry, 'stuff', 'Removed entry callback value wrong')
-
-      test.open(data.key, function (err, entry) {
-        assert.equal(entry, null, 'Removed entry not null when opened')
-        assert.deepEqual(err, {entryDeleted: true}, 'Opening removed entry does not return correct error')
-        console.info('Success: removed string entry')
       })
     })
   })
